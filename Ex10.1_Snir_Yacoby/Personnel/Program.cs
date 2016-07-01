97aee5774b7a570fc7057c151737b2e3c9283589
Snirdude <yacobysn@gmail.com>
2016-07-01 12:10:36 +0300

    10.1 Complete

---
+++ Ex10.1_Snir_Yacoby/Personnel/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Personnel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            List<string> names = p.ReadStringsFromFile();
+
+            foreach(string name in names)
+            {
+                Console.WriteLine("Name: " + name);
+            }
+        }
+
+        List<string> ReadStringsFromFile()
+        {
+            FileStream fs = new FileStream("Names.txt", FileMode.Open, FileAccess.Read);
+            StreamReader reader = new StreamReader(fs);
+            List<string> answer = new List<string>();
+
+            while (!reader.EndOfStream)
+            {
+                answer.Add(reader.ReadLine());
+            }
+
+            reader.Close();
+            fs.Close();
+
+            return answer;
+        }
+    }
+}
