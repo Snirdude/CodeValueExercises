ab0386a53dfaee5dc6ab844d1dab637bc3a098f7
Snirdude <yacobysn@gmail.com>
2016-07-01 11:21:40 +0300

    Adding 9.1

---
+++ Ex9.1_Snir_Yacoby/ShapeLib/Ellipse.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShapeLib
+{
+    public class Ellipse : Shape
+    {
+        public double HorizontalRadius { get; set; }
+        public double VerticalRadius { get; set; }
+
+        public Ellipse(double i_HorizontalR, double i_VerticalR, ConsoleColor i_Color) : base(i_Color)
+        {
+            HorizontalRadius = i_HorizontalR;
+            VerticalRadius = i_VerticalR;
+        }
+
+        public Ellipse(double i_HorizontalR, double i_VerticalR)
+        {
+            HorizontalRadius = i_HorizontalR;
+            VerticalRadius = i_VerticalR;
+        }
+
+        public override double Area
+        {
+            get
+            {
+                return Math.PI * HorizontalRadius * VerticalRadius;
+            }
+        }
+
+        public override void Display()
+        {
+            base.Display();
+            Console.WriteLine("Ellipse:");
+            Console.WriteLine("Horizontal Radius: " + HorizontalRadius);
+            Console.WriteLine("Vertical Radius: " + VerticalRadius);
+        }
+    }
+}
---
+++ Ex9.1_Snir_Yacoby/ShapeLib/Rectangle.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShapeLib
+{
+    public class Rectangle : Shape
+    {
+        public double Height { get; set; }
+        public double Width { get; set; }
+
+        public Rectangle(double i_Height, double i_Width, ConsoleColor i_Color) : base(i_Color)
+        {
+            Height = i_Height;
+            Width = i_Width;
+        }
+
+        public Rectangle(double i_Height, double i_Width)
+        {
+            Height = i_Height;
+            Width = i_Width;
+        }
+
+        public override double Area
+        {
+            get
+            {
+                return Height * Width;
+            }
+        }
+
+        public override void Display()
+        {
+            base.Display();
+            Console.WriteLine("Rectangle:");
+            Console.WriteLine("Height: " + Height);
+            Console.WriteLine("Width: " + Width);
+        }
+    }
+}
