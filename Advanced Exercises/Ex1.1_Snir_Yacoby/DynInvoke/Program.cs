bad98cfc878f1c9807125bbf36c85c04f5b11a4e
Snirdude <yacobysn@gmail.com>
2016-07-10 14:32:07 +0300

    1.1 Complete

--- Advanced Exercises/Ex1.1_Snir_Yacoby/DynInvoke/Program.cs
+++ Advanced Exercises/Ex1.1_Snir_Yacoby/DynInvoke/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Reflection;
 
 namespace DynInvoke
 {
@@ -10,6 +11,19 @@ namespace DynInvoke
     {
         static void Main(string[] args)
         {
+            A a = new A();
+            B b = new B();
+            C c = new C();
+            InvokeHello(a, "Snir");
+            InvokeHello(b, "Snir");
+            InvokeHello(c, "Snir");
+        }
+
+        static void InvokeHello(object obj, string str)
+        {
+            object[] args = new string[1] { str };
+            string str2 = obj?.GetType().InvokeMember("Hello", BindingFlags.Public | BindingFlags.InvokeMethod | BindingFlags.Instance, null, obj, args).ToString();
+            Console.WriteLine(str2);
         }
     }
 }
