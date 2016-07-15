44b2fbda50763f67349ba5eb5597359c2c7bd49f
Snirdude <yacobysn@gmail.com>
2016-07-15 11:08:09 +0300

    4.1

---
+++ Advanced Exercises/Ex4.1_Snir_Yacoby/LINQ to Objects/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ_to_Objects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var assembly = Assembly.GetAssembly(typeof(string));
+            var interfacesNames = from x in assembly.GetTypes()
+                                  where x.IsInterface;
+
+            foreach(string name in interfacesNames)
+            {
+                Console.WriteLine($"Interface Name: {name}, Number of methods: {Type.GetType(name).GetMethods().Count()}");
+            }
+        }
+    }
+}
