9c8c22749631d7dc1bd3e465da64f6cdefaad860
Snirdude <yacobysn@gmail.com>
2016-07-22 10:58:47 +0300

    4.2

---
+++ Ex4.2_Snir_Yacoby/XLinq/Helper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XLinq
+{
+    class Helper
+    {
+        public IEnumerable<Type> GetAllClasses()
+        {
+            return Assembly.GetAssembly(typeof(string)).GetTypes().Where(c => c.IsClass && c.IsPublic);
+        }
+    }
+}
---
+++ Ex4.2_Snir_Yacoby/XLinq/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using System.Reflection;
+
+namespace XLinq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XDocument root = new XDocument();
+            Helper helper = new Helper();
+            IEnumerable<Type> classes = helper.GetAllClasses();
+            foreach (var c in classes)
+            {
+                var properties = c.GetProperties(BindingFlags.Public | BindingFlags.Instance);
+                var methods = c.GetMethods(BindingFlags.Public | BindingFlags.Instance);
+                root.Add(new XElement(new XElement("Type", new XAttribute("Fullname", c.FullName))));
+            }
+        }
+    }
+}
