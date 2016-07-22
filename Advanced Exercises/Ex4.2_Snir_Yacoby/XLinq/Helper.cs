ec07730370874330f1c5e24bb9a4a82a35976174
Snirdude <yacobysn@gmail.com>
2016-07-22 13:04:27 +0300

    4.2 Progress

--- Advanced Exercises/Ex4.2_Snir_Yacoby/XLinq/Helper.cs
+++ Advanced Exercises/Ex4.2_Snir_Yacoby/XLinq/Helper.cs
@@ -4,14 +4,83 @@ using System.Linq;
 using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace XLinq
 {
     class Helper
     {
-        public IEnumerable<Type> GetAllClasses()
+        public XDocument CreateXML()
         {
-            return Assembly.GetAssembly(typeof(string)).GetTypes().Where(c => c.IsClass && c.IsPublic);
+            XDocument document = new XDocument();
+            XElement root = new XElement("Classes");
+            document.Add(root);
+            var classes = Assembly.GetAssembly(typeof(string)).GetTypes().Where(c => c.IsClass && c.IsPublic);
+
+            foreach (var c in classes)
+            {
+                var properties = c.GetProperties(BindingFlags.Public | BindingFlags.Instance);
+                var methods = c.GetMethods(BindingFlags.Public | BindingFlags.Instance);
+                var typeElement = new XElement("Type", new XAttribute("Fullname", c.FullName));
+                var propertiesElement = new XElement("Properties");
+                var methodsElement = new XElement("Methods");
+                foreach (var property in properties)
+                {
+                    propertiesElement.Add(new XElement("Property", new XAttribute("Name", property.Name), new XAttribute("Type", property.PropertyType)));
+                }
+
+                typeElement.Add(propertiesElement);
+                foreach (var method in methods)
+                {
+                    if (c.GetMethods(BindingFlags.DeclaredOnly).Contains(method))
+                    {
+                        continue;
+                    }
+
+                    var parameters = method.GetParameters();
+                    var parametersElement = new XElement("Parameters");
+                    var methodElement = new XElement("Method", new XAttribute("Name", method.Name), new XAttribute("ReturnType", method.ReturnType));
+                    foreach (var parameter in parameters)
+                    {
+                        parametersElement.Add(new XElement("Parameter", new XAttribute("Name", parameter.Name), new XAttribute("Type", parameter.ParameterType)));
+                    }
+                    methodElement.Add(parametersElement);
+                    methodsElement.Add(methodElement);
+                }
+                typeElement.Add(methodsElement);
+                root.Add(typeElement);
+            }
+
+            document.Save("Classes.xml");
+
+            return document;
+        }
+
+        public void A(XDocument document)
+        {
+            var noPropTypes = from d in document.Descendants("Type")
+                              where !d.Descendants("Property").Any()
+                              orderby d.Attribute("Fullname").Value
+                              select d.Attribute("Fullname").Value;
+            foreach (var type in noPropTypes)
+            {
+                Console.WriteLine(type);
+            }
+            Console.WriteLine(noPropTypes.Count());
+        }
+
+        public void B(XDocument document)
+        {
+            var methodsCount = document.Descendants("Method").Count();
+            Console.WriteLine(methodsCount);
+        }
+
+        public void C(XDocument document)
+        {
+            var propertiesCount = document.Descendants("Property").Count();
+            Dictionary<string, int> dictionary = new Dictionary<string, int>();
+            var propertyGroup = from p in document.Descendants("Parameter")
+                                group p by p.Attribute("Type");
         }
     }
 }
--- Advanced Exercises/Ex4.2_Snir_Yacoby/XLinq/Program.cs
+++ Advanced Exercises/Ex4.2_Snir_Yacoby/XLinq/Program.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Xml.Linq;
 using System.Reflection;
+using System.IO;
 
 namespace XLinq
 {
@@ -12,15 +13,9 @@ namespace XLinq
     {
         static void Main(string[] args)
         {
-            XDocument root = new XDocument();
             Helper helper = new Helper();
-            IEnumerable<Type> classes = helper.GetAllClasses();
-            foreach (var c in classes)
-            {
-                var properties = c.GetProperties(BindingFlags.Public | BindingFlags.Instance);
-                var methods = c.GetMethods(BindingFlags.Public | BindingFlags.Instance);
-                root.Add(new XElement(new XElement("Type", new XAttribute("Fullname", c.FullName))));
-            }
+            var document = helper.CreateXML();
+            helper.B(document);
         }
     }
 }
