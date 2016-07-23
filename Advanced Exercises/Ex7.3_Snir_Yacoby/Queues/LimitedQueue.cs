5d1e4446506b7059e5fd5d12c8e5bb808090f553
Snirdude <yacobysn@gmail.com>
2016-07-23 17:58:50 +0300

    Progress

---
+++ Advanced Exercises/Ex7.3_Snir_Yacoby/Queues/LimitedQueue.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Queues
+{
+    class LimitedQueue<T>
+    {
+        private Queue<T> queue;
+        private SemaphoreSlim semaphore;
+
+        public LimitedQueue(int limit)
+        {
+            semaphore = new SemaphoreSlim(limit);
+        }
+
+        public void Enqueue(T item)
+        {
+            semaphore.Wait();
+            queue.Enqueue(item);
+            semaphore.Release();
+        }
+
+        public T Dequeue()
+        {
+            semaphore.Wait();
+            return queue.Dequeue();
+            semaphore.Release();
+        }
+    }
+}
